7805d7373e1b562ce754b6c69d693b41ef43555b
Vesso05 <Vesso05@mail.com>
2019-04-17 14:55:08 +0300

    first commit

---
+++ ClouWay/src/Exceptions/OutofIntervalException.java
@@ -0,0 +1,24 @@
+/**
+ * Custom made Exception
+ */
+
+public class OutofIntervalException extends Exception {
+
+    public OutofIntervalException() {}
+
+    // Constructor that accepts a message
+    public OutofIntervalException(String message)
+    {
+        super(message);
+    }
+
+}
+
+
+
+
+
+
+
+
+
